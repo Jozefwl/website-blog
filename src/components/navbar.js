6a8b42ab2998ce6477c25a8c3cd84b9e7ec5f57d
Jozef W. <jozko.waldo@gmail.com>
2024-08-28 22:34:43 +0200

    Add about page
    Add about page
    Learn something about tailwindcss

---
+++ src/components/navbar.js
@@ -0,0 +1,11 @@
+import '../app/globals.css';
+
+export default function Navbar() {
+  return (
+    <nav className="navbar w-full flex justify-between p-4 font-orbitron">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+    </nav>
+  );
+}
