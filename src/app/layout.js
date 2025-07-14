72387c7979b5b7f533dbf37a7c2ded8fae162323
Jozef Waldhauser <jozef.waldhauser@unicorn.com>
2025-07-14 13:02:37 +0200

    New favicon, description

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import './globals.css';
 import Navbar from '../components/navbar';
 export const metadata = {
   title: "Waldhauser.sk",
-  description: "Created using Next.js",
+  description: "Personal website of Jozef Waldhauser",
 };
 
 export default function RootLayout({ children }) {
