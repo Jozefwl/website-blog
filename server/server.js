8c9f6718c35159496f162e7b5d7524e2bdbddc7b
Jozef Waldhauser <jozef.waldhauser@unicorn.com>
2025-08-31 10:40:01 +0200

    prometheus url env config

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 const port = 5000;
 
-const prometheusUrl = 'http://prometheus-stack-kube-prom-prometheus.default.svc.cluster.local:9090';
+const prometheusUrl = process.env.PROMETHEUS_URL;
 
 // queries
 const cpuQuery = '100 - (avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[5m]))) * 100';
